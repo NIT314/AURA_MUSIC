f608ca1aad3a3f520b1459596a8f3eeece6cd1aa
NIT <gxnit2026@gmail.com>
2026-06-25 12:52:22 +0530

    add custom CORS headers middleware to app.py

--- app.py
+++ app.py
@@ -41,6 +41,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.middleware("http")
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
+
 @app.get("/api/search")
 @limiter.limit("60/minute") # Ek user 1 minute mein max 60 search kar sakta hai
 async def api_search(request: Request, q: str = Query(..., min_length=1), filter: str = None):
